b01352f4a8d5aada7221ee2368321cbdb5f0f440
内山堅司 <s.osamu007@gmail.com>
2021-05-06 09:56:17 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+//        static void Main(string[] args)
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return 123;
+        }
+    }
+}
